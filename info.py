b9f3b28a46bca97185714e030af469cc504cc7f4
Tokyo <rishavx5@gmail.com>
2026-09-25 19:54:15 +0530

    Update info.py

--- info.py
+++ info.py
@@ -31,7 +31,7 @@ INDEX_CAPTION = bool(environ.get('SAVE_CAPTION', True)) # Save caption db when i
 #Making it false will not save caption in db SO you can save some storage space
 
 
-PICS = (environ.get('PICS', 'https://ibb.co/zHjmmDfy')).split()  # Sample pic
+PICS = (environ.get('PICS', 'https://ibb.co/FLzjwFsm')).split()  # Sample pic
 NOR_IMG = environ.get("NOR_IMG", "https://graph.org/file/e20b5fdaf217252964202.jpg")
 MELCOW_PHOTO = environ.get("MELCOW_PHOTO", "https://ibb.co/zHjmmDfy")
 SPELL_IMG = environ.get("SPELL_IMG", "https://graph.org/file/13702ae26fb05df52667c.jpg")
